b7a9d5fe0d7538778123e05fc682a839e41e216f
alanbatikhov <114462467+alanbatikhov@users.noreply.github.com>
2022-09-27 10:48:21 +0600

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,18 @@
+# task1
+#a=input('a=')
+#b=input('b=')
+#print('a=', b)
+#print('b=', a)
+#task2
+# long_burp=lambda n:'Bu'+n*'r'+'p'
+# n=input('r=')
+# print(long_burp)
+# task3
+#txt = input("Insert text: ")
+#def remove_vowels(txt,x):
+#     vowels= 'AEIOUaeiou'
+#     for char in vowels:
+#         txt=txt.replace(char,x)
+#     return txt
+#x="*"
+#print("remove_vowels(",txt,") ->",remove_vowels(txt,x))
